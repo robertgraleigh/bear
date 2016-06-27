154d924e6370ff6e0cb88f0a19d01ede070357a6
robertgraleigh <robertgraleigh@gmail.com>
2016-06-27 09:34:47 -0700

    updated products

--- app/index/index.ctrl.js
+++ app/index/index.ctrl.js
@@ -1,5 +1,5 @@
 (function () {
-  /* GET INDEX page. */
+  /* GET INDEX view. */
   module.exports.index = function(req, res) {
     res.render('index', {
       title: 'Bear - This is an app about the future',
@@ -19,6 +19,40 @@
         {
           mainHeader: 'Section title',
           subHeader: 'This is the section subheader'
+        }
+      ],
+      benefits: [
+        {
+          header: 'Benefit one',
+          description: 'This is th benefit section of thw page.',
+					image: '/img/placeholder.png'
+        },
+        {
+          header: 'Benefit two',
+          description: 'This is th benefit section of thw page.',
+					image: '/img/placeholder.png'
+        },
+        {
+          header: 'Benefit three',
+          description: 'This is th benefit section of thw page.',
+					image: '/img/placeholder.png'
+        }
+      ],
+      products: [
+        {
+          header: 'Product one',
+          description: 'This is the first product of the home page.',
+					image: '/img/placeholder.png'
+        },
+        {
+          header: 'Product two',
+          description: 'This is the second product of the home page.',
+					image: '/img/placeholder.png'
+        },
+        {
+          header: 'Product three',
+          description: 'This is the third product of the home page.',
+					image: '/img/placeholder.png'
         }
       ]
     });
